cfad82880227bad9b24c67b46bcf2c7ecffad028
FaseehFS <faseeh1080@gmail.com>
2026-03-11 09:21:18 +0530

    Create ExternalLink component

---
+++ components/ExternalLink.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+
+type Props = {
+  href: string;
+  children: ReactNode;
+  className?: string;
+};
+
+export default function ExternalLink({ href, children, className }: Props) {
+  return (
+    <a
+      href={href}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={`text-blue-600 hover:underline ${className || ""}`}
+    >
+      {children}
+    </a>
+  );
+}
