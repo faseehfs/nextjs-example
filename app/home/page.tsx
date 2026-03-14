0403da5de5df58d849caafdda4dba84aaf34febe
FaseehFS <faseeh1080@gmail.com>
2026-03-14 09:42:12 +0530

    Use api

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  return (
-    <div className="w-full p-4 mt-8">
-      <h1 className="text-4xl text-center mb-4">Work in progress 🏗️</h1>
-      <p className="text-center">
-        This page is currently work in progress. Please return later.
-      </p>
-    </div>
-  );
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return <p className="text-center m-4">{message}</p>;
 }
