56a5b5317a5a047b37d097cf194f77419bffca8f
FaseehFS <faseeh1080@gmail.com>
2026-03-25 17:39:00 +0530

    style: move sign out button to user menu

--- app/(user)/components/navbar.tsx
+++ app/(user)/components/navbar.tsx
@@ -93,7 +93,6 @@ function NavContents() {
       <NavLink href="/home">Home</NavLink>
       <NavLink href="/wip">WIP</NavLink>
       <NavLink href="/test">Tests</NavLink>
-      <SignOutButton />
     </>
   );
 }
@@ -106,35 +105,6 @@ function NavLink({ href, children }: { href: string; children: ReactNode }) {
     >
       {children}
     </Link>
-  );
-}
-
-function SignOutButton() {
-  return (
-    <AlertDialog>
-      <AlertDialogTrigger asChild>
-        <Button variant="outline">Sign Out</Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Do you want to sign out?</AlertDialogTitle>
-          <AlertDialogDescription>
-            If you sign out, you will have to sign in again.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction
-            variant="destructive"
-            onClick={() => {
-              signOut();
-            }}
-          >
-            Sign Out
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
   );
 }
 
@@ -157,16 +127,74 @@ function UserMenu() {
       </DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
-          <DrawerTitle>User actions</DrawerTitle>
-          <DrawerDescription>A lkav ko iw lakj wie oiae.</DrawerDescription>
+          <DrawerTitle className="hidden">User menu</DrawerTitle>
+          <UserMenuContent />
         </DrawerHeader>
         <DrawerFooter>
-          <Button>Submit</Button>
           <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
+            <Button variant="outline">Close</Button>
           </DrawerClose>
         </DrawerFooter>
       </DrawerContent>
     </Drawer>
+  );
+}
+
+function UserMenuContent() {
+  const { data: session } = useSession();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex gap-4 pb-4 border-b border-border">
+        {session?.user?.image && (
+          <Image
+            src={session.user.image}
+            width={240}
+            height={240}
+            alt="User Avatar"
+            referrerPolicy="no-referrer"
+            className="w-20 rounded-full"
+          />
+        )}
+        <div className="flex flex-col gap-2">
+          <h1 className="text-2xl">{session?.user?.name}</h1>
+          <p className="text-muted-foreground">{session?.user?.email}</p>
+        </div>
+      </div>
+      <p>
+        This is your User Menu. There is nothing much here other than the sign
+        out button, but I will add stuff later.
+      </p>
+      <SignOutButton />
+    </div>
+  );
+}
+
+function SignOutButton() {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="destructive">Sign Out</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Do you want to sign out?</AlertDialogTitle>
+          <AlertDialogDescription>
+            If you sign out, you will have to sign in again.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            variant="destructive"
+            onClick={() => {
+              signOut();
+            }}
+          >
+            Sign Out
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 }
