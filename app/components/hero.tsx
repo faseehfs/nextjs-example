b3e4e897747ae3a111766dfc5a80e1bef282fa93
FaseehFS <faseeh1080@gmail.com>
2026-03-29 12:32:13 +0530

    feat: bring back the old landing page

---
+++ app/components/hero.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+export default function Hero() {
+  return (
+    <header
+      className="flex flex-col justify-center items-center p-5 h-80 bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/csias-lighthouse.png')" }}
+    >
+      <h1 className="text-5xl text-center text-white">Landing Page</h1>
+      <p className="text-center text-white my-2">
+        Description of the website description of it hello there
+      </p>
+      <Link href="/home">
+        <Button size="lg">Open App</Button>
+      </Link>
+    </header>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,77 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import Hero from "./components/hero";
+import Feature from "@/components/ui/feature";
+import ExternalLink from "@/components/ui/external-link";
+import CardSec from "@/components/ui/card-sec";
 import Link from "next/link";
-import FSImage from "@/assets/fs-nobg-cropped.png";
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+import sweetHomeImage from "@/assets/sweet-home.png";
+import sourceCodeImage from "@/assets/source-code.png";
+import nextjsImage from "@/assets/next-js.png";
+import webSearchImage from "@/assets/web-search.png";
+import workingAtHomeImage from "@/assets/working-at-home.png";
+import imagesImage from "@/assets/images.png";
+import landingPageImage from "@/assets/landing-page.png";
+import waitingForYouImage from "@/assets/waiting-for-you.png";
 
 export default function LandingPage() {
   return (
-    <div className="flex  w-full min-h-[75vh] justify-center items-center p-4">
-      <Card className="w-full max-w-sm">
-        <CardHeader>
-          <CardTitle className="text-center mb-2">
-            Welcome to Next.js Example!
-          </CardTitle>
-          <CardDescription className="text-center">
-            Hey, thanks for visiting. This is my Next.js example application
-            which I made to learn Next.js.
-          </CardDescription>
-        </CardHeader>
-        <CardFooter>
-          <Link href="/home" className="w-full">
-            <Button className="w-full">Open App</Button>
+    <main className="bg-white">
+      <Hero />
+
+      <div className="mx-auto w-full max-w-4xl px-5">
+        <Feature
+          image={webSearchImage}
+          title="What is this?"
+          text="This is a Next.js application."
+        />
+
+        <hr className="text-border" />
+
+        <Feature
+          image={workingAtHomeImage}
+          title="Why did you make this?"
+          text="To learn Next.js"
+          reverse
+        />
+
+        <hr className="text-border" />
+
+        <Feature
+          image={imagesImage}
+          title="From where did you get these images?"
+        >
+          <ExternalLink href="https://undraw.co/">Undraw.co</ExternalLink>
+        </Feature>
+
+        <hr className="text-border" />
+
+        <Feature
+          image={landingPageImage}
+          title="What about the banner image?"
+          reverse
+        >
+          <ExternalLink href="https://pixabay.com/">Pixabay</ExternalLink>
+        </Feature>
+      </div>
+
+      <div className="flex flex-col gap-8 p-8 justify-center items-center w-full bg-gray-100">
+        <h1 className="text-center text-xl">What's in here</h1>
+        <div className="grid gap-8 grid-cols-1 sm:grid-cols-3 max-w-4xl">
+          <CardSec image={sweetHomeImage} title="Pages" />
+          <CardSec image={sourceCodeImage} title="Code" />
+          <CardSec image={nextjsImage} title="Next.js" />
+        </div>
+      </div>
+
+      <div className="mx-auto w-full max-w-4xl px-5">
+        <Feature image={waitingForYouImage} title="What should I do now?">
+          {" "}
+          <Link href="/home">
+            <Button>Open App</Button>
           </Link>
-        </CardFooter>
-      </Card>
-    </div>
+        </Feature>
+      </div>
+    </main>
   );
 }
