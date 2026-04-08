0349dc345586e628480fb1b78e7894ff2c85414c
FaseehFS <faseeh1080@gmail.com>
2026-04-08 16:45:41 +0530

    add combined animations

--- app/(user)/tailwind-animations/page.tsx
+++ app/(user)/tailwind-animations/page.tsx
@@ -1,3 +1,4 @@
+import Article from "@/components/ui/article";
 import {
   Table,
   TableBody,
@@ -9,48 +10,64 @@ import {
 
 export default function TailwindAnimations() {
   return (
-    <Table className="max-w-sm mx-auto mt-4">
-      <TableHeader>
-        <TableRow>
-          <TableHead>className</TableHead>
-          <TableHead>Example</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        <TableRow>
-          <TableCell className="font-mono">animate-bounce</TableCell>
-          <TableCell className="text-xl animate-bounce">😀</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell>
-            <span className="font-mono">animate-bounce</span> with{" "}
-            <span className="font-mono">delay</span>
-          </TableCell>
-          <TableCell>
-            <div className="flex">
-              <p className="text-xl animate-bounce delay-0">😁</p>
-              <p className="text-xl animate-bounce delay-150">😁</p>
-              <p className="text-xl animate-bounce delay-300">😁</p>
-            </div>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-mono">animate-ping</TableCell>
-          <TableCell className="relative">
-            <p className="text-xl absolute animate-ping bottom-2">😶‍🌫️</p>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-mono">animate-pulse</TableCell>
-          <TableCell className="text-xl animate-pulse">🥵</TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell className="font-mono">animate-spin</TableCell>
-          <TableCell className="relative">
-            <p className="text-xl absolute bottom-2 animate-spin">😵‍💫</p>
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+    <div className="flex flex-col gap-4 max-w-xl mx-auto mt-4">
+      <Article>
+        <h1>Tailwind animations</h1>
+        <p>
+          Tailwind provides utility classes to animate your elements easily.
+        </p>
+      </Article>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>className</TableHead>
+            <TableHead>Example</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          <TableRow>
+            <TableCell className="font-mono">animate-bounce</TableCell>
+            <TableCell className="text-xl animate-bounce">😀</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell>
+              <span className="font-mono">animate-bounce</span> with{" "}
+              <span className="font-mono">delay</span>
+            </TableCell>
+            <TableCell>
+              <div className="flex">
+                <p className="text-xl animate-bounce delay-0">😁</p>
+                <p className="text-xl animate-bounce delay-150">😁</p>
+                <p className="text-xl animate-bounce delay-300">😁</p>
+              </div>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-mono">animate-ping</TableCell>
+            <TableCell className="relative">
+              <p className="text-xl absolute animate-ping bottom-2">😶‍🌫️</p>
+            </TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-mono">animate-pulse</TableCell>
+            <TableCell className="text-xl animate-pulse">🥵</TableCell>
+          </TableRow>
+          <TableRow>
+            <TableCell className="font-mono">animate-spin</TableCell>
+            <TableCell className="relative">
+              <p className="text-xl absolute bottom-2 animate-spin">😵‍💫</p>
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+      <p>
+        Animations can also be combined to create even more fun animations, like
+        this:
+      </p>
+      <div className="animate-bounce">
+        <div className="w-min text-xl animate-spin">😵</div>
+      </div>
+      <p>But you should put the content in nested divs.</p>
+    </div>
   );
 }
