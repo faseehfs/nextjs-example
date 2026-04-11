304ad7b61edfb74078f659c3b620cb6ef3119be0
FaseehFS <faseeh1080@gmail.com>
2026-04-11 21:43:33 +0530

    fix: prisma error

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { prisma } from "./lib/prisma";
+import prisma from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
