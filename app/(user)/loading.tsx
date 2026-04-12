20c74c3a5abe9c8d3b3fee330e64dd6fa84af7bf
FaseehFS <faseeh1080@gmail.com>
2026-04-12 21:26:10 +0530

    Revert "make things centered"

--- app/(user)/loading.tsx
+++ app/(user)/loading.tsx
@@ -2,7 +2,7 @@ import LoadingDots from "@/components/ui/loading-dots";
 
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen w-full">
+    <div className="flex items-center justify-center min-h-[75vh] w-full">
       <LoadingDots />
     </div>
   );
